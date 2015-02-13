d5123f0eb42f182e2b8e943fc31c0c189490afc1
Ghost1227 <dgriffiths@section214.com>
2015-02-13 09:48:21 -0600

    First round settings tests

---
+++ tests/tests-settings.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @group meetup_connect_settings
+ */
+class Test_Settings extends WP_UnitTestCase {
+    public function setUp() {
+        parent::setUp();
+    }
+
+    public function tearDown() {
+        parent::tearDown();
+    }
+
+    public function test_get_settings_tabs() {
+        $this->assertArrayHasKey( 'settings', meetup_connect_get_settings_tabs() );
+    }
+
+    public function test_get_registered_settings() {
+        $settings = meetup_connect_get_registered_settings();
+
+        $this->assertArrayHasKey( 'auth_type', $settings['settings'] );
+    }
+}
