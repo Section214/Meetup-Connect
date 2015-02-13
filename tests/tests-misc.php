52e2ebaa38db83f3202c30945866c04c14cbe60a
Ghost1227 <dgriffiths@section214.com>
2015-02-13 01:35:22 -0600

    Test unit tests

---
+++ tests/tests-misc.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @group meetup_connect_misc
+ */
+class Test_Misc extends WP_UnitTestCase {
+    public function setUp() {
+        parent::setUp();
+    }
+
+    public function tearDown() {
+        parent::tearDown();
+    }
+
+    public function test_sample() {
+        $foo = 'bar';
+        $this->assertEquals( 'bar', $foo );
+    }
+}
